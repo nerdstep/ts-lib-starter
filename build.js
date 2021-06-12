8b0391c34774e1a1859044a7fa68dd43cb723ed3
Justin Williams <justinwilliams42@gmail.com>
2021-06-12 08:08:52 -0700

    chore: update deps; replace build with estrella

--- build.js
+++ build.js
@@ -1,22 +1,49 @@
-const esbuild = require('esbuild')
+// @ts-check
+const { build, log, tsconfig } = require('estrella')
+const ts = require('typescript')
 const pkg = require('./package.json')
 
-function build(args) {
-  esbuild
-    .build({
-      entryPoints: ['src/index.ts'],
-      outfile: `dist/${pkg.name}.js`,
-      bundle: true,
-      format: 'esm',
-      target: 'es2017',
-      watch: args.includes('--watch'),
-      external: [
-        ...Object.keys(pkg.devDependencies || {}),
-        ...Object.keys(pkg.optionalDependencies || {}),
-        ...Object.keys(pkg.peerDependencies || {}),
-      ],
-    })
-    .catch(() => process.exit(1))
+function generateTypeDefs(tsconfig, entry) {
+  log.info(`Generating type declaration files for ${entry.join(', ')}`)
+
+  const { options: compilerOptions } = ts.parseJsonConfigFileContent(
+    tsconfig,
+    ts.sys,
+    process.cwd()
+  )
+
+  compilerOptions.declaration = true
+  compilerOptions.emitDeclarationOnly = true
+
+  //log.info(compilerOptions)
+
+  const program = ts.createProgram(entry, compilerOptions)
+  const emit = program.emit()
+
+  //log.info(emit)
+
+  for (const { file, messageText } of emit.diagnostics) {
+    if (typeof messageText === 'string') {
+      if (file) log.warn(`=> ${file.fileName}`)
+      log.warn(messageText)
+    } else {
+      log.warn(messageText.messageText)
+    }
+  }
 }
 
-build(process.argv.slice(2))
+build({
+  entry: 'src/index.ts',
+  outfile: `dist/${pkg.name}.js`,
+  bundle: true,
+  platform: 'node',
+  target: ['es2017'],
+  external: [
+    ...Object.keys(pkg.devDependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
+  onEnd: (config) => {
+    //log.info(config)
+    generateTypeDefs(tsconfig(config), config.entry)
+  },
+})
